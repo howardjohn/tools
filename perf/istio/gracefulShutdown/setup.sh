886eaaa5a3008835d9600a9d987102b6ce2b2868
John Howard <howardjohn@google.com>
2019-02-14 15:53:41 -0800

    Replace fortio

--- perf/istio/gracefulShutdown/setup.sh
+++ perf/istio/gracefulShutdown/setup.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-set -ex
+set -e
+
+function set_connection_duration() {
+  # TERMINATION_DRAIN_DURATION_SECONDS is not easily accessible currently as it must be manually added to the sidecar injector configmap.
+  # If this is set to custom value, we should require the duration value to be set to match it. Otherwise, we default to 4s.
+
+  termination=$(kubectl -n istio-system get configmap istio-sidecar-injector -o jsonpath='{.data.config}')
+
+  if $(echo ${termination} | grep -q TERMINATION_DRAIN_DURATION_SECONDS); then
+    connectionDuration=${DURATION:?"If a custom TERMINATION_DRAIN_DURATION_SECONDS is set, then DURATION must be provided. This should be set to a value slight (1s) below the termination duration."}
+  else
+    connectionDuration=4 # Default drain duration is 5 seconds
+  fi;
+}
 
 function install_all_config() {
   local DIRNAME="${1:?"output dir"}"
@@ -8,9 +21,11 @@ function install_all_config() {
 
   kubectl create ns graceful-shutdown || true
 
-  kubectl label namespace graceful-shutdown istio-injection=enabled || true
+  kubectl label namespace graceful-shutdown istio-injection=enabled --overwrite || true
 
-  helm -n graceful-shutdown template . > "${OUTFILE}"
+  set_connection_duration
+
+  helm -n graceful-shutdown template . --set connectionDuration=${connectionDuration:?} > "${OUTFILE}"
 
   if [[ -z "${DRY_RUN}" ]]; then
       kubectl -n graceful-shutdown apply -f "${OUTFILE}"
